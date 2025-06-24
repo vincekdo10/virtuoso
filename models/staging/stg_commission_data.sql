c4ec0ebac9d10affcad7366117990e1e25b4b80f
vincekdo10 <vdo1017@gmail.com>
2025-06-24 13:18:30 -0500

    Add files via upload

---
+++ models/staging/stg_commission_data.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='dynamic_table',
+    on_configuration_change='apply',
+    target_lag='downstream',
+    snowflake_warehouse='WH_PIPE_COMMISSION'
+) }}
+
+with source_data as (
+    select
+        commissionid,
+        scbookingid,
+        datasourceid
+    from {{ source('commission', 'commission_materialized_withuuid') }}
+),
+
+transformed as (
+    select
+        commissionid,
+        scbookingid,
+        datasourceid,
+        -- Add computed columns for staging
+        lower(trim(commissionid)) as commission_id_clean,
+        case 
+            when datasourceid is not null then true 
+            else false 
+        end as has_data_source
+    from source_data
+)
+
+select * from transformed 
